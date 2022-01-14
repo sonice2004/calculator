b387d09f0530d999776fda7f82ee882c41ba22d3
sonice2004 <solomon.tolosa@gmail.com>
2022-01-13 20:37:08 -0500

    calculator module added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import {React, useState} from 'react';
+import Calculator from "./Calculator";
+import History from "./History";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App row">
+      <div class="col-xl-6">
+        <Calculator/>
+      </div>
+      <div className="col-xl-6">
+        <History/>
+      </div>
     </div>
   );
 }
---
+++ src/History.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+const History = () => {
+    return (  
+      <div className="history-container">
+      <h1>History</h1> 
+      <div className ="history border border-primary rounded" >           
+              <div><label className = "expression"> 1+2 = 3</label></div> 
+              <div><label className = "expression"> 1+2 = 3</label></div>
+              <div><label className = "expression"> 1+2 = 3</label></div>
+              <div><label className = "expression"> 1+2 = 3</label></div>   
+              <div><label className = "expression"> 1+2 = 3</label></div>
+              <div><label className = "expression"> 1+2 = 3</label></div>
+      </div>
+      </div>
+       );
+  
+};
+
+export default History;
